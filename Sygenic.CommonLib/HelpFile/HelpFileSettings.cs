f4ed35a85275bdd8d9d23d6f66fd6cc86a4503a7
cybersan <cybersan@gmail.com>
2023-12-02 15:53:49 +0100

    - help, - console, - environment

--- Sygenic.CommonLib/HelpFile/HelpFileSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sygenic.CommonLib;
-
-public sealed class HelpFileSettings
-{
-	public bool DisplayHelpFile { get; set; } = true;
-	public string HelpFilePath { get; set; } = "";
-}
--- Sygenic.CommonLib/HelpFile/IHelpFile.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Sygenic.CommonLib;
-
-public interface IHelpFile
-{
-	void MaybeDisplayHelpFile();
-}
